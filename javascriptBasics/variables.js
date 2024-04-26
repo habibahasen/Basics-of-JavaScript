1b8bfd41294df6b01c3876dceb741d4b145acadb
Hana <hababihasen@gmail.com>
2024-04-26 21:06:18 +0300

    changed updated variables.js

--- javascriptBasics/variables.js
+++ javascriptBasics/variables.js
@@ -9,7 +9,7 @@ variable = undefined;
 let pi = 3.14;
 console.log(typeof(variable));
 console.log(variable);
-console.log(pi);
+// console.log(pi);
 
 
 // Basic operations
@@ -17,4 +17,16 @@ console.log(pi);
 let value1, value2;
 value1 = 25;
 value2 = 5;
+var sum = value1 + value2; //Addition
+
+string1 = "This is ";
+string2 = "'Concatenation'"
+concat = string1 + string2;  //concatenation
+console.log(sum);
+console.log(concat);
+
+var multiply = value1 * value2;
+console.log(multiply);
+ 
+
 
