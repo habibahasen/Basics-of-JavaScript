357894e7dce98e744f92f21493d27cd6ed1c4a95
Hana <hababihasen@gmail.com>
2024-04-26 20:26:57 +0300

    added intro to js

---
+++ javascriptBasics/variables.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+// Declaring variables and Assigning  Values
+var variable; // if we use var we can assign any variable type we want
+variable = 2;
+variable = "changeable";
+variable = 2 > 5;
+variable = undefined;
+let pi = 3.14;
+console.log(typeof(variable));
+console.log(variable);
+console.log(pi);
+
+
+// Basic operations
+
+let value1, value2;
+value1 = 25;
+value2 = 5;
+
