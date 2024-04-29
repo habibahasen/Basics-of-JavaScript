bbaa2455d4bfb0376b94aee167a1aefe9f1c2e8c
Hana <hababihasen@gmail.com>
2024-04-29 16:40:48 +0300

    added loops

---
+++ javascriptBasics/loops.js
@@ -0,0 +1,7 @@
+//for loop
+let number =  5;
+console.log(`Multiplication Table for ${number}`);
+for(let i =1; i <=10; i++){
+    let result = number * i;
+    console.log(`${number} X ${i} = ${result}`);
+}
