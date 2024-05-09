e4992f557e9305d3c31a9459cb564fd6e1495c68
Hana <hababihasen@gmail.com>
2024-05-09 21:54:37 +0300

    modified loops

--- javascriptBasics/loops.js
+++ javascriptBasics/loops.js
@@ -3,13 +3,13 @@ let number =  7;
 console.log(`Multiplication Table for ${number}`);
 for(let i =1; i <=7; i++){
     let result = number * i;
-    console.log(`${number} X ${i} = ${result}`);
+    // console.log(`${number} X ${i} = ${result}`);
 }
 //while loop
 let i = 1;
 while(i <8){
     let result = number*i;
-    console.log(`${number} X ${i}= ${result}`);
+    // console.log(`${number} X ${i}= ${result}`);
     i++;
 }
 
@@ -23,7 +23,7 @@ do{
     userInput = readlineSync.question("Enter a positive number:");
     num = parseFloat(userInput);
 }while (isNaN(num) || num <=0);
-console.log(`you entered a valid positive number ${num}`);
+// console.log(`you entered a valid positive number ${num}`);
 
 //iterate over array
 
@@ -31,29 +31,45 @@ const scores = [22,54,76,92,43,33];
 let j = 0;
 //while
 while(j < scores.length){
-    console.log(scores[j]);
+    // console.log(scores[j]);
     j++;
 }
 //do ...while
 do{
-    console.log(scores[j])
+    // console.log(scores[j])
     j++;
 }while(j< scores.length);
 
 //
 let k = 6;
 do {
-    console.log(scores[k]);
+    //console.log(scores[k]);
     k++;
 }while(k < scores.length)
 
 //for 1
 for(let m = 0; m < scores.length; m++){
-    console.log(scores[m]);
+    // console.log(scores[m]);
 }
 
 // for ...in
 for (i in scores){
-    console.log(scores[i]);
+    // console.log(scores[i]);
 }
 
+//iterate over objects
+let person ={
+    fname :'Haba',
+    sname : 'nesro',
+    age : 23,
+    hobbies : ['reading', 'drawing', 'writing poem'],
+}
+for(key in person){
+    // console.log(key ,person[key])
+    
+}
+const dictLength = Object.keys(person).length
+while(dictLength <7){
+    console.log(`The number of key-value pairs is  ${dictLength}`)
+    break;
+}
