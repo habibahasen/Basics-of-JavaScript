9fb1259438b2df07131bacb98e7b0619cd6fb3cf
Hana <hababihasen@gmail.com>
2024-05-22 20:04:47 +0300

    added objects

---
+++ javascriptBasics/objects.js
@@ -0,0 +1,12 @@
+const person = {
+    fname : 'Entu',
+    lname : 'Babi',
+    hobbies : ['ustaza', 'cook', 'read', 'learn_language'],
+    siblings : {
+        olders : ['tof', 'haba'],
+        youngers : ['hanuna', 'abu','samu']
+    },
+    age : 20
+}
+person.age = 21;
+console.log(person['age']);
