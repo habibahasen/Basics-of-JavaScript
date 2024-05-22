547a96060efa7e0876a341de865eba6e02328eb4
Hana <hababihasen@gmail.com>
2024-05-22 20:41:11 +0300

    updated objects

--- javascriptBasics/objects.js
+++ javascriptBasics/objects.js
@@ -6,7 +6,15 @@ const person = {
         olders : ['tof', 'haba'],
         youngers : ['hanuna', 'abu','samu']
     },
+    salamet: function() {
+        console.log("princess says Asalamu alaykum!")
+    },
     age : 20
 }
+delete person.siblings.olders[1];
+person.siblings.olders.splice(1,1);
 person.age = 21;
-console.log(person['age']);
+person.student = true;
+console.log(person.siblings)
+console.log(person.hobbies.slice(-2));
+person.salamet();
